f0b1fd8dc8eb19eb95abd12b28c4db3fd44af5f0
Srushti Gol <105737505+Srushti-Gol@users.noreply.github.com>
2024-03-09 22:24:05 +0530

    changes for checking

--- app.py
+++ app.py
@@ -9,17 +9,17 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 def hello_world():
     return 'Hello from Srushti'
  
-CropRecModel = joblib.load('./models/CropRecModel.joblib')
-@app.route('/predictCrop', methods=['POST'])
-def predict_crop():
-    data = request.get_json()
-    # Convert values to float
-    features = [float(data[key]) for key in ['Nitrogen', 'Phosphorous', 'Potassium', 'Temperature', 'Humidity', 'ph', 'Rainfall']]
+# CropRecModel = joblib.load('./models/CropRecModel.joblib')
+# @app.route('/predictCrop', methods=['POST'])
+# def predict_crop():
+#     data = request.get_json()
+#     # Convert values to float
+#     features = [float(data[key]) for key in ['Nitrogen', 'Phosphorous', 'Potassium', 'Temperature', 'Humidity', 'ph', 'Rainfall']]
     
-    # Print the features
-    print("Features:", features)
+#     # Print the features
+#     print("Features:", features)
 
-    # Make prediction
-    prediction = CropRecModel.predict([features])
+#     # Make prediction
+#     prediction = CropRecModel.predict([features])
     
-    return jsonify({"Prediction" : prediction[0]}), 200
+#     return jsonify({"Prediction" : prediction[0]}), 200
