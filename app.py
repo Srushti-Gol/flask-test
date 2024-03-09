0421cc656fda188ec2c74fa7ee0377fd3240dc2a
Srushti Gol <105737505+Srushti-Gol@users.noreply.github.com>
2024-03-09 21:51:45 +0530

    add crop recommendation model

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask,request, jsonify
+import joblib
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello from Srushti'
  
+CropRecModel = joblib.load('./models/CropRecModel.joblib')
+@app.route('/predictCrop', methods=['POST'])
+def predict_crop():
+    data = request.get_json()
+    # Convert values to float
+    features = [float(data[key]) for key in ['Nitrogen', 'Phosphorous', 'Potassium', 'Temperature', 'Humidity', 'ph', 'Rainfall']]
+    
+    # Print the features
+    print("Features:", features)
+
+    # Make prediction
+    prediction = CropRecModel.predict([features])
+    
+    return jsonify({"Prediction" : prediction[0]}), 200
